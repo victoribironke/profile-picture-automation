6cc6af3f54763f9b31b72bbcf8baa26428c2b48f
Victor Ibironke <victoribironke2019@gmail.com>
2024-06-10 23:54:58 +0100

    fixed it finally

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,12 @@
 import { adventurerNeutral } from "@dicebear/collection";
 import { createAvatar } from "@dicebear/core";
-import sharp from "sharp";
-import { TwitterApi } from "twitter-api-v2";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
+import axios from "axios";
+import OAuth from "oauth-1.0a";
+import { createHmac } from "crypto";
+import sharp from "sharp";
 config();
 const app = express();
 app.use(cors());
@@ -18,34 +20,53 @@ const SECRETS = {
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
 };
-const twitterClient = new TwitterApi({
-    appKey: SECRETS.apiKey,
-    appSecret: SECRETS.apiKeySecret,
-    accessToken: SECRETS.accessToken,
-    accessSecret: SECRETS.accessTokenSecret,
-});
+const API_KEY = SECRETS.apiKey;
+const API_SECRET_KEY = SECRETS.apiKeySecret;
+const ACCESS_TOKEN = SECRETS.accessToken;
+const ACCESS_TOKEN_SECRET = SECRETS.accessTokenSecret;
 const getAvatar = () => createAvatar(adventurerNeutral, {
     seed: Date.now().toString(),
     flip: true,
 });
-const saveImage = (str) => sharp(Buffer.from(str))
-    .png()
-    .toFile("output.png", (err, info) => {
-    if (err) {
-        console.error("Error converting SVG to PNG:", err);
-    }
-    else {
-        console.log("SVG successfully converted to PNG:", info);
-    }
-});
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     try {
-        const twitterAvatar = getAvatar();
-        saveImage(twitterAvatar.toString());
-        setTimeout(async () => await twitterClient.v1.updateAccountProfileImage("output.png"), 1000);
+        const twitterAvatar = getAvatar().toString();
+        const oauth = new OAuth({
+            consumer: { key: API_KEY, secret: API_SECRET_KEY },
+            signature_method: "HMAC-SHA1",
+            hash_function: (base_string, key) => {
+                return createHmac("sha1", key).update(base_string).digest("base64");
+            },
+        });
+        const token = {
+            key: ACCESS_TOKEN,
+            secret: ACCESS_TOKEN_SECRET,
+        };
+        const svgBuffer = Buffer.from(twitterAvatar);
+        const buffer = await sharp(svgBuffer).png().toBuffer();
+        const image = buffer.toString("base64");
+        const request_data = {
+            url: "https://api.twitter.com/1.1/account/update_profile_image.json",
+            method: "POST",
+            data: {
+                image,
+            },
+        };
+        const headers = {
+            ...oauth.toHeader(oauth.authorize(request_data, token)),
+            "Content-Type": "application/x-www-form-urlencoded",
+        };
+        const response = await axios({
+            url: request_data.url,
+            method: request_data.method,
+            headers,
+            data: new URLSearchParams(request_data.data).toString(),
+        });
+        console.log("Profile image updated successfully", response.data);
         res.status(200).json({ data: "Successful" });
     }
     catch (e) {
+        console.error("Error updating profile image", e.response.data);
         res.status(500).json({ error: "An error occured" });
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { adventurerNeutral } from "@dicebear/collection";
 import { createAvatar } from "@dicebear/core";
-import sharp from "sharp";
-import { TwitterApi } from "twitter-api-v2";
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
+import axios from "axios";
+import OAuth from "oauth-1.0a";
+import { createHmac } from "crypto";
+import sharp from "sharp";
 
 config();
 
@@ -23,12 +25,10 @@ const SECRETS = {
   clientSecret: process.env.CLIENT_SECRET!,
 };
 
-const twitterClient = new TwitterApi({
-  appKey: SECRETS.apiKey,
-  appSecret: SECRETS.apiKeySecret,
-  accessToken: SECRETS.accessToken,
-  accessSecret: SECRETS.accessTokenSecret,
-});
+const API_KEY = SECRETS.apiKey;
+const API_SECRET_KEY = SECRETS.apiKeySecret;
+const ACCESS_TOKEN = SECRETS.accessToken;
+const ACCESS_TOKEN_SECRET = SECRETS.accessTokenSecret;
 
 const getAvatar = () =>
   createAvatar(adventurerNeutral, {
@@ -36,29 +36,52 @@ const getAvatar = () =>
     flip: true,
   });
 
-const saveImage = (str: string) =>
-  sharp(Buffer.from(str))
-    .png()
-    .toFile("output.png", (err, info) => {
-      if (err) {
-        console.error("Error converting SVG to PNG:", err);
-      } else {
-        console.log("SVG successfully converted to PNG:", info);
-      }
+app.get("/", async (req, res) => {
+  try {
+    const twitterAvatar = getAvatar().toString();
+
+    const oauth = new OAuth({
+      consumer: { key: API_KEY, secret: API_SECRET_KEY },
+      signature_method: "HMAC-SHA1",
+      hash_function: (base_string, key) => {
+        return createHmac("sha1", key).update(base_string).digest("base64");
+      },
     });
 
-app.get("/", (req, res) => {
-  try {
-    const twitterAvatar = getAvatar();
-    saveImage(twitterAvatar.toString());
-    setTimeout(
-      async () =>
-        await twitterClient.v1.updateAccountProfileImage("output.png"),
-      1000
-    );
+    const token = {
+      key: ACCESS_TOKEN,
+      secret: ACCESS_TOKEN_SECRET,
+    };
+
+    const svgBuffer = Buffer.from(twitterAvatar);
+    const buffer = await sharp(svgBuffer).png().toBuffer();
+    const image = buffer.toString("base64");
+
+    const request_data = {
+      url: "https://api.twitter.com/1.1/account/update_profile_image.json",
+      method: "POST",
+      data: {
+        image,
+      },
+    };
+
+    const headers = {
+      ...oauth.toHeader(oauth.authorize(request_data, token)),
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+
+    const response = await axios({
+      url: request_data.url,
+      method: request_data.method,
+      headers,
+      data: new URLSearchParams(request_data.data).toString(),
+    });
+
+    console.log("Profile image updated successfully", response.data);
 
     res.status(200).json({ data: "Successful" });
   } catch (e) {
+    console.error("Error updating profile image", e.response.data);
     res.status(500).json({ error: "An error occured" });
   }
 });
